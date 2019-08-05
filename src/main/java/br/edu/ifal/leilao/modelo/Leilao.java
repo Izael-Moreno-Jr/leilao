9f461070751f79dd4289639e09b941d6403340dc
Izael-Moreno-Jr <izaelmorenojr17@gmail.com>
2019-08-05 15:23:47 -0300

    Instanciando uma lista de lances no contrutor vazio da classe 'Leilao' e criacao do metodo 'prpoe' passando um parametro (lances) e adicionando um lance

--- src/main/java/br/edu/ifal/leilao/modelo/Leilao.java
+++ src/main/java/br/edu/ifal/leilao/modelo/Leilao.java
@@ -1,20 +1,27 @@
 package br.edu.ifal.leilao.modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Leilao {
 	
-	private List<Lance> lances;
+	private ArrayList<Lance> lances;
 
 	public Leilao() {
 		super();
+		this.lances = new ArrayList<Lance>();
 	}
+
 
 	public List<Lance> getLances() {
 		return lances;
 	}
 
 	public void setLances(List<Lance> lances) {
-		this.lances = lances;
+		this.lances = (ArrayList<Lance>) lances;
+	}
+	
+	public void propoe(Lance lances) {
+		this.lances.add(lances);
 	}
 }
